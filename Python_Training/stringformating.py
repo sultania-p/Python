362a8794375889b7f4c868fa5a44bdcddcef05a8
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2021-10-11 20:16:00 -0400

    string formatting functions

---
+++ Python_Training/stringformating.py
@@ -0,0 +1,15 @@
+age = 30
+print("My age is " + str(age) + " years")
+print("My age is {0} years".format(age))    # coerce.. replacing the {} values with the age variable
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7} ".format("31", "January", "March", "May", "July", "August", "October", "December")
+      +"months")
+
+print("""
+Jan : {1}
+Feb : {0}
+Mar : {1}
+Apr : {2}""".format(28,31,30))
+
+# String Formatting
+
